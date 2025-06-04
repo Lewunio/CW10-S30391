0c40b026f8a506bf9868139ef9fe57054a79f24b
Maciej Lewandowski <s30391@pjwstk.edu.pl>
2025-06-04 19:09:50 +0200

    Wstęp

---
+++ CW10-S30391/Program.cs
@@ -0,0 +1,27 @@
+using CW10_S30391.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<MasterContext>(opt =>
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
